159deb9b42960489e47853b67360dc1d32447f97
정정욱 <jeonguk29@naver.com>
2025-05-09 15:19:28 +0900

    build: 초기 프로젝트 셋팅

---
+++ HOLIX_iOS/HOLIX_iOS/App/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  HOLIX_iOS
+//
+//  Created by 정정욱 on 5/9/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
---
+++ HOLIX_iOS/HOLIX_iOS/View/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HOLIX_iOS
+//
+//  Created by 정정욱 on 5/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
