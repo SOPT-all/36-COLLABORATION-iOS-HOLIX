1f6c79924547b0dee406a6af2a19b836ddc03951
정정욱 <jeonguk29@naver.com>
2025-05-09 17:12:11 +0900

    build: SwiftFormat 설정

--- HOLIX_iOS/HOLIX_iOS/App/SceneDelegate.swift
+++ HOLIX_iOS/HOLIX_iOS/App/SceneDelegate.swift
@@ -10,13 +10,13 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+
         let rootViewController = ViewController()
         let navigationController = UINavigationController(rootViewController: rootViewController)
-        
+
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
