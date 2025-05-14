91b4a960cb7def3ef458363500459a5807d0e783
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-05-14 10:54:09 +0900

    feat: CustomNavigationBar 컴포넌트화 작업

---
+++ HOLIX_iOS/HOLIX_iOS/Global/Component/CustomNavigationBar.swift
@@ -0,0 +1,127 @@
+//
+//  CustomNavigationBar.swift
+//  HOLIX_iOS
+//
+//  Created by 임재현 on 5/14/25.
+//
+
+import UIKit
+import SnapKit
+
+final class CustomNavigationBar: UIView {
+    
+    // MARK: - Properties
+    
+    private var haveMenuButton: Bool = false
+    
+    // MARK: - UI Components
+    
+    private let backButton = UIButton()
+    private let titleLabel = UILabel()
+    private let searchButton = UIButton()
+    private let menuButton = UIButton()
+    private let buttonStackView = UIStackView()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(titleLabel: String = "", haveMenuButton: Bool) {
+        super.init(frame: .zero)
+        self.haveMenuButton = haveMenuButton
+        self.titleLabel.text = titleLabel
+        setupAddSubview()
+        setupUI()
+        setStyle()
+    }
+    
+    // MARK: - SetupAddSubview
+    
+    private func setupAddSubview() {
+        [backButton,titleLabel,buttonStackView].forEach {
+            self.addSubview($0)
+        }
+        
+        self.buttonStackView.addArrangedSubview(searchButton)
+        
+        if haveMenuButton {
+            self.buttonStackView.addArrangedSubview(menuButton)
+            menuButton.snp.makeConstraints {
+                $0.size.equalTo(30)
+            }
+        }
+    }
+    // MARK: - SetupUI
+    
+    private func setupUI() {
+        backButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.size.equalTo(20)
+            $0.centerY.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(self.backButton.snp.trailing).offset(16)
+            $0.centerY.equalToSuperview()
+        }
+        
+        buttonStackView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.size.equalTo(30)
+        }
+    }
+    
+    // MARK: - SetStyle
+    
+    private func setStyle() {
+        self.backgroundColor = .white
+        backButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        backButton.tintColor = .black
+        
+        titleLabel.textColor = .black
+        titleLabel.font = .pretendard(size: 16)
+        
+        searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.tintColor = .black
+        menuButton.setImage(UIImage(systemName: "line.3.horizontal"), for: .normal)
+        menuButton.tintColor = .black
+        
+        buttonStackView.axis = .horizontal
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.spacing = 5
+        
+    }
+    
+    // MARK: - SetAddTarget
+    
+    private func setAddTarget() {
+        backButton.addTarget(self, action: #selector(backButtonDidTap), for: .touchUpInside)
+        searchButton.addTarget(self, action: #selector(searchButtonDidTap), for: .touchUpInside)
+        menuButton.addTarget(self, action: #selector(menuButtonDidTap), for: .touchUpInside)
+
+    }
+    
+    // MARK: - Button Actions
+    
+    @objc private func backButtonDidTap() {
+        print("backButton 클릭됨")
+    }
+    
+    @objc private func searchButtonDidTap() {
+        print("searchButton 클릭됨")
+    }
+    
+    @objc private func menuButtonDidTap() {
+        print("menuButton 클릭됨")
+    }
+}
--- HOLIX_iOS/HOLIX_iOS/View/ViewController.swift
+++ HOLIX_iOS/HOLIX_iOS/View/ViewController.swift
@@ -6,11 +6,24 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    private let customNavigationBar = CustomNavigationBar(haveMenuButton: true)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setupUI()
     }
-
+    
+    private func setupUI() {
+        self.view.addSubview(customNavigationBar)
+        
+        customNavigationBar.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(-20)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+    }
 }
