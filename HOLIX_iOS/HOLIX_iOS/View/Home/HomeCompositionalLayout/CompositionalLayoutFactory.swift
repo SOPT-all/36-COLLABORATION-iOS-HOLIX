b184f909aca32c014e9349d518ab9ea74c06bce1
정정욱 <jeonguk29@naver.com>
2025-05-15 19:20:16 +0900

    feat: 초기 CompositionalLayoutFactory 구현

---
+++ HOLIX_iOS/HOLIX_iOS/View/Home/HomeCompositionalLayout/CompositionalLayoutFactory.swift
@@ -0,0 +1,54 @@
+//
+//  CompositionalLayoutFactory.swift
+//  HOLIX_iOS
+//
+//  Created by 정정욱 on 5/15/25.
+//
+
+import UIKit
+
+enum HomeSectionType: Int, CaseIterable {
+    case banner = 0
+//    case categoryMenu
+//    case popularStudy
+//    case recommendedContent
+}
+
+struct CompositionalLayoutFactory {
+
+    static func create() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { sectionNumber, _ in
+            let section: NSCollectionLayoutSection
+
+            switch HomeSectionType(rawValue: sectionNumber) {
+            case .banner:
+                section = makeBannerSection()
+            case .none:
+                section = makeBannerSection()
+            }
+
+            return section
+        }
+    }
+
+    private static func makeBannerSection() -> NSCollectionLayoutSection {
+
+        // 아이템 사이즈
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        // 그룹 사이즈
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(382))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        // 섹션 사이즈
+        let section = NSCollectionLayoutSection(group: group) // 섹션의 크기는 내부 그룹이 결정
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        section.orthogonalScrollingBehavior = .paging
+        return section
+    }
+}
