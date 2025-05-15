251467ee35510bf6a77c0dbe373517087d39ffc4
정정욱 <jeonguk29@naver.com>
2025-05-15 19:22:41 +0900

    chore: HomeVC에 CompositionalLayout 등록

--- HOLIX_iOS/HOLIX_iOS/View/Home/HomeViewController.swift
+++ HOLIX_iOS/HOLIX_iOS/View/Home/HomeViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+import SnapKit
+import Then
+
+final class HomeViewController: UIViewController {
+
+    // MARK: - Properties
+
+    private let bannerData = BannerResponse.mockData()
 
     // MARK: - UI Components
 
-    private let topTabBar = TopTabBarView(items: ["추천", "강의", "스터디", "북클럽", "멘토링", "커뮤니티"])
+    private lazy var homeCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: self.createCompositionalLayout()
+    )
 
     // MARK: - LifeCycle
 
@@ -20,27 +30,74 @@ class HomeViewController: UIViewController {
         setUp()
         setStyle()
         setLayout()
+        setRegister()
     }
 
     // MARK: - Setup
 
-    func setUp() {
-        view.addSubview(topTabBar)
+    private func setUp() {
+        view.addSubviews(homeCollectionView)
     }
 
     // MARK: - SetStyle
 
-    func setStyle() {
-
+    private func setStyle() {
+        homeCollectionView.do {
+            $0.backgroundColor = .white
+            $0.dataSource = self
+        }
     }
 
     // MARK: - SetLayout
 
-    func setLayout() {
-        topTabBar.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(40)
+    private func setLayout() {
+        homeCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    // MARK: - SetRegister
+
+    private func setRegister() {
+        homeCollectionView.do {
+            $0.register(BannerCell.self, forCellWithReuseIdentifier: BannerCell.identifier)
+        }
+    }
+
+    // MARK: - Layout Configuration
+
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return CompositionalLayoutFactory.create()
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return HomeSectionType.allCases.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let sectionType = HomeSectionType(rawValue: section) else { return 0 }
+        switch sectionType {
+        case .banner:
+            return bannerData.count
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let sectionType = HomeSectionType(rawValue: indexPath.section) else {
+            return UICollectionViewCell()
+        }
+
+        switch sectionType {
+        case .banner:
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: BannerCell.identifier,
+                for: indexPath) as! BannerCell
+            cell.configure(image: bannerData[indexPath.item].Image)
+            return cell
         }
     }
 }
--- HOLIX_iOS/HOLIX_iOS/View/TestViewController.swift
+++ HOLIX_iOS/HOLIX_iOS/View/TestViewController.swift
@@ -17,16 +17,16 @@ class TestViewController: UIViewController {
         setStyle()
         customNavigationBar.delegate = self
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
     }
-    
+
     private func setAddSubview() {
         self.view.addSubview(customNavigationBar)
     }
-    
+
     private func setupUI() {
         customNavigationBar.snp.makeConstraints {
             $0.top.equalTo(self.view.safeAreaLayoutGuide)
@@ -34,23 +34,23 @@ class TestViewController: UIViewController {
             $0.height.equalTo(50)
         }
     }
-    
+
     private func setStyle() {
         self.view.backgroundColor = .white
     }
 }
 
 extension TestViewController: CustomNavigationBarDelegate {
-    
+
     func didTapBackButton() {
         print("VC에서 받음 backButton Actions")
         self.navigationController?.popViewController(animated: true)
     }
-    
+
     func didTapSearchButton() {
         print("VC에서 받음 searchButton Actions")
     }
-    
+
     func didTapMenuButton() {
         print("VC에서 받음 menuButton Actions")
     }
