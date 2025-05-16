d196cafe0b0f33b2b994316ac2d37d52ba6b24d6
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-05-16 17:55:36 +0900

    feat: BubbleLabelView 구현

---
+++ HOLIX_iOS/HOLIX_iOS/View/Chatting/ChattingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingViewController.swift
+//  HOLIX_iOS
+//
+//  Created by 임재현 on 5/16/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ChattingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
