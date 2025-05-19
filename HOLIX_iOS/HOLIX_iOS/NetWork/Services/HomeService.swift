9bae88a243dc8f22dfec026ca774e0b49ed36ef5
정정욱 <jeonguk29@naver.com>
2025-05-19 20:14:23 +0900

    feat: HomeService 구현

---
+++ HOLIX_iOS/HOLIX_iOS/NetWork/Services/HomeService.swift
@@ -0,0 +1,20 @@
+//
+//  HomeService.swift
+//  HOLIX_iOS
+//
+//  Created by 정정욱 on 5/19/25.
+//
+
+import Foundation
+
+final class HomeService {
+
+    static let shared = HomeService()
+
+    func getMain() async throws -> StudyResponse {
+        return try await APIService.shared.request(
+            path: Endpoint.Main.getMain.path,
+            method: Endpoint.Main.getMain.method,
+            responseType: StudyResponse.self)
+    }
+}
